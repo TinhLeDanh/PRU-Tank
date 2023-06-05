541d7935240498eedb8e5088f0578c2b2ae17339
Chu Van Trai (k15 HL) <traicvhe153014@fpt.edu.vn>
2023-06-06 00:29:33 +0700

    update get object by json

--- Assets/Scripts/CRUDData/Chage.cs
+++ Assets/Scripts/CRUDData/Chage.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
 
 public class Chage : MonoBehaviour
@@ -21,21 +23,21 @@ public class Chage : MonoBehaviour
 
     public void OnButtonClick()
     {
-        // string filePath = Path.Combine("Save", "InforTank.json");
+    }
+    public T GetObjectByJson<T>(string key)
+    {
         string filePath =  Application.dataPath + "/Save/InforTank.json";
         string jsonString = File.ReadAllText(filePath);
-        Debug.Log(jsonString);
-        var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
-Debug.Log(data);
-        // Lấy giá trị của key "t1"
-        var t1Data = (Dictionary<string, object>)data["t0"];
-        var t1Name = (string)t1Data["module"];
-        var t1Price = (bool)t1Data["sourceMap"];
-        Debug.Log(t1Name+t1Price);
-        // string t1Name = (string)t1Data["name"];
-        // int t1Price = (int)t1Data["price"];
-        //
-        // Debug.Log("t1 name: " + t1Name);
-        // Debug.Log("t1 price: " + t1Price);
+        JObject jsonObject = JObject.Parse(jsonString);
+        string t0Json = jsonObject[key].ToString();
+        var myObj =JsonConvert.DeserializeObject<T>(t0Json);
+        return myObj;
+    }
+    
+    [Serializable]
+    public class t0
+    {
+        public string module { get; set; }
+        public bool sourceMap { get; set; }
     }
 }
