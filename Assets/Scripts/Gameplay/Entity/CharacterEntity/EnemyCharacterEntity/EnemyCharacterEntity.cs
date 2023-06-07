7013d3515b24217abce81300239addc0a89aab18
Khiếu Vinh An <ankv@aisolutionsjsc.com>
2023-06-08 03:22:11 +0700

    pull main

--- Assets/Scripts/Gameplay/Entity/CharacterEntity/EnemyCharacterEntity/EnemyCharacterEntity.cs
+++ Assets/Scripts/Gameplay/Entity/CharacterEntity/EnemyCharacterEntity/EnemyCharacterEntity.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyCharacterEntity : MonoBehaviour
 {
     public int[,] _spawnPosition;
     public GameObject _tankBot;
+
+    public float duration;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +30,10 @@ public class EnemyCharacterEntity : MonoBehaviour
     private void AutoMovement()
     {
 
+    }
+
+    private void AutoShooting()
+    {
+        
     }
 }
