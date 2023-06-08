c436f935a6afd244062ead03e31aad2b42f095b9
pallgree <hungvt164@gmail.com>
2023-06-09 02:42:04 +0700

    add enter bullet and stuff

--- Assets/Scripts/Gameplay/Entity/CharacterEntity/BulletController.cs
+++ Assets/Scripts/Gameplay/Entity/CharacterEntity/BulletController.cs
@@ -59,4 +59,16 @@ public class BulletController : MonoBehaviour
                 throw new ArgumentOutOfRangeException();
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "steel")
+        {
+            Destroy(gameObject);
+        }else if (collision.gameObject.tag == "brick")
+        {
+            Destroy(gameObject);
+        }
+            
+    }
 }
