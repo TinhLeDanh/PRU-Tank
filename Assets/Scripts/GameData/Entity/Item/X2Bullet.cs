9570762663ef529ede21719a4a6cd96313a5d29b
traicv <traicv@nvoaon.asia>
2023-07-12 11:57:42 +0700

    chá»ore: add Sprites item

---
+++ Assets/Scripts/GameData/Entity/Item/X2Bullet.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace GameData.Entity.Item
+{
+    [CreateAssetMenu(menuName = GameConst.X2Bullet_FileName, fileName = GameConst.X2Bullet_MenuName, order = GameConst.X2Bullet_Order)]
+    public class X2Bullet : BaseItem
+    {
+        public int NumberBullet { get; set; }
+    }
+}
