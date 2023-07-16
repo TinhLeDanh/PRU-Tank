89f44d8b700af9b669b1688ec8119529f3d2304c
Khiếu Vinh An <ankv@aisolutionsjsc.com>
2023-07-16 19:55:15 +0700

    restore entity

--- Assets/Scripts/Gameplay/Entity/CharacterEntity/EnemyCharacterEntity/EnemyCharacterEntity.cs
+++ Assets/Scripts/Gameplay/Entity/CharacterEntity/EnemyCharacterEntity/EnemyCharacterEntity.cs
@@ -9,10 +9,14 @@ public class EnemyCharacterEntity : MonoBehaviour
     public GameObject _tankBot;
 
     public float duration;
+    
+    // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    // Update is called once per frame
     void Update()
     {
         
